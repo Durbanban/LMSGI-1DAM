e201b714f19da0be4a03a4cea63b882d887a471c
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-03-22 14:28:13 +0100

    Añadido el boletín 8

--- EJERCICIOS/Ejemplos HTML/tema8/js/scripts.js
+++ EJERCICIOS/Ejemplos HTML/tema8/js/scripts.js
@@ -1,1 +1,5 @@
-alert("La programación es mi pasión"); alert("Tu vieja"); // Los alert son los equivalentes a los sysos en java.
+alert("La programación es mi pasión"); alert("Tu vieja"); // Los alert son los equivalentes a los sysos
+let variable = 13;
+
+alert(variable === "13"); // Por defecto, vamos a trabajar con el triple igual '===' (comparación estricta), ya que tiene en cuenta tipo y valor de la variable, además es la que se utiliza epara entrar en cada case del switch
+

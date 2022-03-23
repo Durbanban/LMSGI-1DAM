0c3fbf309b8f037b206e5c4ffbe2fe873cb634c5
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-03-23 09:01:47 +0100

    Hasta el ejercicio 5 hecho

---
+++ BOLETINES/tema8/ejercicio03/js/scripts.js
@@ -0,0 +1,11 @@
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+for (let mes of meses) {
+    alert(mes);
+};
+
+/* Otra forma de hacerlo (como en java)
+for (let i = 0; i < meses.length; i++) {
+    alert(meses[i]);
+    
+}
+*/

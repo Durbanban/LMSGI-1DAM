b351b933e20b4c9567b26097c7f35f75c0252cbc
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-04-01 09:28:41 +0200

    Perfeccionado el ejercicio en clase

--- EJERCICIOS/EjercicioEnClase/js/scripts.js
+++ EJERCICIOS/EjercicioEnClase/js/scripts.js
@@ -4,19 +4,16 @@ let suma = 0;
 let tope = +prompt("¿Qué tamaño quiere que tenga el array?");
     do {
         num = +prompt("Introduzca un número positivo");
-        if (num >= 0) {
+        if (isFinite(num) && num >= 0) {
             if (conjunto.length == tope) {
-                conjunto.shift();
+                suma -= conjunto.shift();
+                suma += num;
                 conjunto.push(num);
             }else {
+                suma += num;
                 conjunto.push(num);
             }
-            for (let i = 0; i < conjunto.length; i++) {
-                suma += conjunto[i];
-                
-            }
-            alert(conjunto + "Todo suma: " + suma);
-            suma = 0;
+            alert(conjunto + " y todo suma: " + suma);
             
         }
-    }while (num >= 0);
+    }while (isFinite(num) && num >= 0);
